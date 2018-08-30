e5d4377b4fb4df61ee0790afc2b80c15cd173584
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-30 13:46:27 +0300

    Update bc.js

--- bc.js
+++ bc.js
@@ -300,6 +300,33 @@ Server Count: __${guild.memberCount}__**`)//DarkLast Alpha Codes
        }
 
    });
+
+
+client.on('message', message => {
+
+            if(!message.channel.guild) return;
+
+let args = message.content.split(' ').slice(1).join(' ');
+
+if (message.content.startsWith('.bcall')){
+
+ if (message.author.id !== '470500980342128650') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+
+client.users.forEach(m =>{
+
+m.sendMessage(args)
+
+})
+
+}
+
+});
+
+
+
+
    
 
 client.login(process.env.BOT_TOKEN); 
